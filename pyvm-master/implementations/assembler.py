071a33ce0b24cf3f7bf7d05c3c7a2e46a9886805
Piscenco M <32290501+piscenco@users.noreply.github.com>
2021-04-11 20:47:43 +0300

    rm

--- pyvm-master/implementations/assembler.py
+++
@@ -1,9 +0,0 @@
-class Assembler:
-    def __init__(self):
-        pass
-
-    def assemble(self, asm_file_name):
-        asm_file_text = open(asm_file_name, encoding="utf-8")
-        for i, line in enumerate(asm_file_text):
-            cmd, arg1, arg2, arg3 = line.replace('\n', '').split(' ')
-            print(cmd, arg1, arg2)
