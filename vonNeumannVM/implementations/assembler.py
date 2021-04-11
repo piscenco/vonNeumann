29ed4c3e913e63149a10cae7528209efc8f6d656
Piscenco M <32290501+piscenco@users.noreply.github.com>
2021-04-11 23:23:36 +0300

    added compilation of code

--- vonNeumannVM/implementations/assembler.py
+++ vonNeumannVM/implementations/assembler.py
@@ -20,16 +20,23 @@ class Assembler:
                 for j in range(len(variables_names)):
                     {variables_names[j]: self.end}.update(self.variables)
                     encoded_var = variables_names[j].encode("cp1251")
-                    self._binaryCode[self.end * 4: self.end * 4 + len(encoded_var)] = encoded_var
-                    self.end = self.end * 4 + len(encoded_var)
+                    self.binary_code[self.end * 4: self.end * 4 + len(encoded_var)] = encoded_var
+                    self.end += len(encoded_var)//4
                 continue
-            new_cmd, arg1, arg2, arg3 = line.replace('\n', '').split(' ')
-
-        asm_file_text.close()
-        asm_file_text = open(asm_file_name, encoding="utf-8")
-        for i, line in enumerate(asm_file_text):
-            new_cmd, arg1, arg2, arg3 = line.replace('\n', '').split(' ')
-
+            new_cmd, arg0, arg1, arg2 = line.replace('\n', '').split(' ')
+            if arg0 != 0:
+                arg0 = variables_names[arg0]
+            if arg1 != 0:
+                arg1 = variables_names[arg1]
+            if arg2 != 0:
+                arg2 = variables_names[arg2]
+            byteCode = bytearray(4)
+            byteCode[0:1] = self.list_of_commands[new_cmd].to_bytes(1, 'big')
+            byteCode[1:2] = variables_names.to_bytes(1, 'big')
+            byteCode[2:3] = arg1.to_bytes(1, 'big')
+            byteCode[3:4] = arg2.to_bytes(1, 'big')
+            self.binary_code[self.end * 4: self.end * 4 + len(encoded_var)] = byteCode
+            self.end += 1
         asm_file_text.close()
 
 
@@ -38,17 +45,5 @@ class Assembler:
         out_file.write(self.binary_code)
         out_file.close()
 
-    def fransformCmd(self, cmd_name, arg0, arg1, arg2):
-        pass
-
 '''UTF-32 (32-bit Unicode Transformation Format) is a fixed-length encoding used to 
 encode Unicode code points that uses exactly 32 bits (four bytes) per code point '''
-byteCode = bytearray(4)
-		byteCode[0:1] = command.command.to_bytes(1, CommonByteOrder)
-		byteCode[1:2] = command.localPtr.to_bytes(1, CommonByteOrder)
-		byteCode[2:4] = command.farPtr.to_bytes(2, CommonByteOrder)
-length = len(valueBytes)
-		assert(length % 4 == 0)
-		offset = self._cellsUsed * 4
-		self._cellsUsed += length // 4
-		self._binaryCode[offset: offset + length] = valueBytes
