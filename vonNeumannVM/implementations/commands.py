7d6065176f91f78d4e288092e8e279402094bb22
Piscenco M <32290501+piscenco@users.noreply.github.com>
2021-04-11 20:48:28 +0300

    commands & code example

---
+++ vonNeumannVM/implementations/commands.py
@@ -0,0 +1,16 @@
+class Commands:
+    def __init__(self):
+      self.list =  {'begin_program': 0x00,
+                    'readln':0x01,
+                    'call':0x02,
+                    'print':0x03,
+                    'function':0x04,
+                    'arg':0x05,
+                    'plus':0x06,
+                    'minus':0x07,
+                    'if_less_than_two':0x08,
+                    'label_true':0x09,
+                    'label_false':0x10,
+                    'end_function':0x11,
+                    'end_program':0x12}
+
