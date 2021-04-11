5e32582b16578f93a8e2ca69327547821ba2dd74
Piscenco M <32290501+piscenco@users.noreply.github.com>
2021-04-11 22:50:21 +0300

    changes to assembler

--- vonNeumannVM/implementations/assembler.py
+++ vonNeumannVM/implementations/assembler.py
@@ -1,9 +1,44 @@
+from commands import Commands
+
+
 class Assembler:
     def __init__(self):
-        pass
+        cmds = Commands()
+        self.list_of_commands = cmds.getCmds()
+        self.binary_code = bytearray(1024)
 
     def assemble(self, asm_file_name):
+        number_of_lines = 0;
+        asm_file_text = open(asm_file_name, encoding="utf-8")
+
+        # find labels' line number
+        for i, line in enumerate(asm_file_text):
+            new_cmd, arg1, arg2, arg3 = line.replace('\n', '').split(' ')
+
+        asm_file_text.close()
         asm_file_text = open(asm_file_name, encoding="utf-8")
         for i, line in enumerate(asm_file_text):
-            cmd, arg1, arg2, arg3 = line.replace('\n', '').split(' ')
-            print(cmd, arg1, arg2)
+            new_cmd, arg1, arg2, arg3 = line.replace('\n', '').split(' ')
+
+        asm_file_text.close()
+
+
+    def writeInOutputFile(self, out_file_name):
+        out_file = open(out_file_name, "wb") # "wb" write in binary mode
+        out_file.write(self.binary_code)
+        out_file.close()
+
+    def fransformCmd(self, cmd_name, arg0, arg1, arg2):
+        pass
+
+'''UTF-32 (32-bit Unicode Transformation Format) is a fixed-length encoding used to 
+encode Unicode code points that uses exactly 32 bits (four bytes) per code point '''
+byteCode = bytearray(4)
+		byteCode[0:1] = command.command.to_bytes(1, CommonByteOrder)
+		byteCode[1:2] = command.localPtr.to_bytes(1, CommonByteOrder)
+		byteCode[2:4] = command.farPtr.to_bytes(2, CommonByteOrder)
+length = len(valueBytes)
+		assert(length % 4 == 0)
+		offset = self._cellsUsed * 4
+		self._cellsUsed += length // 4
+		self._binaryCode[offset: offset + length] = valueBytes
